649bfdd4d67013d2f813260d6fe9abeeba293192
hari hara sankar <90249023+harisankar01@users.noreply.github.com>
2022-01-22 22:15:20 +0530

    secured api key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const API_URL = 'https://api.rawg.io/api';
-const API_KEY = 'cb56eed0e68c4622b7311c3bac19f215';
-console.log(API_KEY);
+const API_KEY = // api key
 const SEARCH_BASE_URL = `${API_URL}/games?key=${API_KEY}`;
 const POPULAR_BASE_URL = `${API_URL}/games?key=${API_KEY}`;
 // For login and voting
